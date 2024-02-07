15b85961b12c0d36901bfc8ba0bf0fa0d180a21a
lucindalaurent <lucindalaurent21@gmail.com>
2024-02-07 14:08:17 +0700

    add unit tests for searchById, edit, and delete feature

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -63,4 +63,123 @@ public class ProductRepositoryTest {
         assertEquals(product2.getProductId(), savedProduct.getProductId());
         assertFalse(productIterator.hasNext());
     }
+
+    @Test
+    void testCreateThenSearchById(){
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("a0f9de46-98b1-437d-a0bf-d0821dde9896");
+        product2.setProductName("Sampo Cap Usep");
+        product2.setProductQuantity(50);
+        productRepository.create(product2);
+
+        Product foundProduct = productRepository.searchById(product1.getProductId());
+        assertEquals(product1.getProductName(), foundProduct.getProductName());
+        assertEquals(product1.getProductQuantity(), foundProduct.getProductQuantity());
+    }
+
+    @Test
+    void testNotFoundById(){
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Exception raisedException = assertThrows(ProductRepository.NoSuchProductException.class, () -> {
+            productRepository.searchById("not-real-id");
+        });
+
+        String expectedMessage = "Product with id not-real-id does not exist!";
+        String actualMessage = raisedException.getMessage();
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    void testCreateThenEdit(){
+        Product realProduct = new Product();
+        realProduct.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd9");
+        realProduct.setProductName("Sampo Cap Dua Hati");
+        realProduct.setProductQuantity(100);
+        productRepository.create(realProduct);
+
+        Product editedProduct = new Product();
+        editedProduct.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd9");
+        editedProduct.setProductName("Sampo Cap Satu Hati");
+        editedProduct.setProductQuantity(10);
+        productRepository.update(editedProduct);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product latestProduct = productIterator.next();
+        assertEquals(realProduct.getProductId(), latestProduct.getProductId());
+        assertEquals(realProduct.getProductName(), latestProduct.getProductName());
+        assertEquals(realProduct.getProductQuantity(), latestProduct.getProductQuantity());
+    }
+
+    @Test
+    void testEditProductNotFound(){
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+        productRepository.create(product);
+
+        Product editedProduct = new Product();
+        editedProduct.setProductId("not-real-id");
+        editedProduct.setProductName("Sampo Cap Odin");
+        editedProduct.setProductQuantity(200);
+
+        assertThrows(ProductRepository.NoSuchProductException.class, () -> {
+            productRepository.update(editedProduct);
+        });
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product remainingProduct = productIterator.next();
+        assertEquals(product.getProductId(), remainingProduct.getProductId());
+        assertEquals(product.getProductName(), remainingProduct.getProductName());
+        assertEquals(product.getProductQuantity(), remainingProduct.getProductQuantity());
+    }
+
+    @Test
+    void testCreateThenDelete() {
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-468e-8860-71af6af63bd6");
+        product1.setProductName("Kecap Cap Naga");
+        product1.setProductQuantity(100);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("a0f9de46-98b1-437d-a0bf-d0821dde9896");
+        product2.setProductName("Sampo Anti Rontok");
+        product2.setProductQuantity(50);
+        productRepository.create(product2);
+
+        productRepository.delete(product1.getProductId());
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product remainingProduct = productIterator.next();
+        assertEquals(product2.getProductId(), remainingProduct.getProductId());
+        assertFalse(productIterator.hasNext());
+    }
+
+    @Test
+    void testDeleteProductNotFound(){
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-468e-8a8u-71af6af63bd6");
+        product.setProductName("Susu Kotak");
+        product.setProductQuantity(20);
+        productRepository.create(product);
+
+        assertThrows(ProductRepository.NoSuchProductException.class, () -> {
+            productRepository.delete("not-a-real-id");
+        });
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+    }
 }
