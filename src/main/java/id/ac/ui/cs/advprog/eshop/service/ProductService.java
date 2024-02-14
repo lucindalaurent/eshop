7a5a1497f93d89875f9944da28a6b85005cb507e
lucindalaurent <lucindalaurent21@gmail.com>
2024-02-14 14:24:55 +0700

    remove unnecessary modifier 'public' in interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product searchById(String id);
-    public Product update(Product editedProduct);
-    public void delete(String productId);
+    Product create(Product product);
+    List<Product> findAll();
+    Product searchById(String id);
+    Product update(Product editedProduct);
+    void delete(String productId);
 }
