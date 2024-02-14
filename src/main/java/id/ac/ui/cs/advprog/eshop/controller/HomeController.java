030114384172647bf953f617392b550443092c0f
lucindalaurent <lucindalaurent21@gmail.com>
2024-02-14 16:32:28 +0700

    fix home controller

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
     @GetMapping("")
     public String homePage() {
-        return "homePage";
+        return "HomePage";
     }
 }
