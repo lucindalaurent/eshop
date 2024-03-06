34bc92005b6b285d24b1988bb547efae56ecb07d
lucindalaurent <lucindalaurent21@gmail.com>
2024-03-06 20:25:00 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -9,18 +9,46 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            //replace existing payment
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+        paymentData.add(payment);
         return payment;
     }
     public Payment findById(String id){
+        for (Payment payment : paymentData){
+            if (payment.getId().equals(id)){
+                return payment;
+            }
+        }
         return null;
     }
     public List<Payment> findAllByVoucherCode(){
-        return null;
+        List<Payment> vcResult = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equals("VOUCHER_CODE")) {
+                vcResult.add(savedPayment);
+            }
+        }
+        return vcResult;
     }
     public List<Payment> findAllByCashOnDelivery(){
-        return null;
+        List<Payment> codResult = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equals("CASH_ON_DELIVERY")) {
+                codResult.add(savedPayment);
+            }
+        }
+        return codResult;
     }
     public List<Payment> getAllPayment(){
-        return null;
+        return paymentData;
     }
 }
