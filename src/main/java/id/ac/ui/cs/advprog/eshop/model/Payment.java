2b01de27b9038db3716810179d2db73efc8b1b56
lucindalaurent <lucindalaurent21@gmail.com>
2024-03-06 19:40:13 +0700

    [GREEN] implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,11 +1,10 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.Map;
 
-@Builder
+
 @Getter
 public class Payment {
     String id;
@@ -14,12 +13,81 @@ public class Payment {
     Map<String, String> paymentData;
     Order order;
 
-    public Payment(String id, String method, Order oder, Map<String, String> paymentData){
+    public Payment(String id, String method, Order order, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (order == null) {
+            throw new IllegalArgumentException();
+        }
+
+        boolean valid = true;
+        switch (method) {
+            case "VOUCHER_CODE":
+                String voucherCode = paymentData.get("voucherCode");
+
+                if (voucherCode != null
+                        && voucherCode.length() == 16
+                        && voucherCode.startsWith("ESHOP")) {
+                    int numCharCount = 0;
+                    for (int i = 0; i < voucherCode.length(); i++) {
+                        if (Character.isDigit(voucherCode.charAt(i))) {
+                            numCharCount++;
+                        }
+                    }
+                    if (numCharCount != 8) {
+                        valid = false;
+                    }
+                } else{
+                    valid = false;
+                }
+                break;
+
+            case "CASH_ON_DELIVERY":
+                String address = paymentData.get("address");
+                String deliveryFee = paymentData.get("deliveryFee");
+
+                if (address == null || address.isEmpty()
+                        || deliveryFee == null || deliveryFee.isEmpty()) {
+                    valid = false;
+                }
+                break;
+
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        if (valid) {
+            setStatus("SUCCESS");
+        } else {
+            setStatus("REJECTED");
+        }
     }
 
-    public Payment(String id, String method, String status, Order order, Map<String, String> paymentData){
+    public Payment(String id, String method, String status, Order order, Map<String, String> paymentData) {
+        this.id = id;
+        this.order = order;
+        this.method = method;
+        this.paymentData = paymentData;
+        setStatus(status);
     }
 
-    public void setStatus(String status){
+    public void setStatus(String status) {
+        if (status.equals("SUCCESS") || status.equals("REJECTED")) {
+            this.status = status;
+            if (status.equals("SUCCESS")) {
+                order.setStatus("SUCCESS");
+            } else {
+                order.setStatus("FAILED");
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
