9463cd71159e89309d0fee43f11aa4062458d7f4
lucindalaurent <lucindalaurent21@gmail.com>
2024-03-06 20:26:08 +0700

    [REFACTOR] PaymentRepository to use a Map instead of a List

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,52 +3,33 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PaymentRepository {
-    private List<Payment> paymentData = new ArrayList<>();
+    //private List<Payment> paymentData = new ArrayList<>();
+    private Map<String, Payment> paymentData = new HashMap<>();
 
     public Payment save(Payment payment) {
-        int i = 0;
-        for (Payment savedPayment : paymentData) {
-            //replace existing payment
-            if (savedPayment.getId().equals(payment.getId())) {
-                paymentData.remove(i);
-                paymentData.add(i, payment);
-                return payment;
-            }
-            i += 1;
-        }
-        paymentData.add(payment);
+        paymentData.put(payment.getId(), payment);
         return payment;
     }
     public Payment findById(String id){
-        for (Payment payment : paymentData){
-            if (payment.getId().equals(id)){
-                return payment;
-            }
-        }
-        return null;
+        return paymentData.get(id);
     }
     public List<Payment> findAllByVoucherCode(){
-        List<Payment> vcResult = new ArrayList<>();
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getMethod().equals("VOUCHER_CODE")) {
-                vcResult.add(savedPayment);
-            }
-        }
-        return vcResult;
+        return paymentData.values().stream()
+                .filter(payment -> payment.getMethod().equals("VOUCHER_CODE"))
+                .collect(Collectors.toList());
     }
     public List<Payment> findAllByCashOnDelivery(){
-        List<Payment> codResult = new ArrayList<>();
-        for (Payment savedPayment : paymentData) {
-            if (savedPayment.getMethod().equals("CASH_ON_DELIVERY")) {
-                codResult.add(savedPayment);
-            }
-        }
-        return codResult;
+        return paymentData.values().stream()
+                .filter(payment -> payment.getMethod().equals("CASH_ON_DELIVERY"))
+                .collect(Collectors.toList());
     }
     public List<Payment> getAllPayment(){
-        return paymentData;
+        return new ArrayList<>(paymentData.values());
     }
 }
